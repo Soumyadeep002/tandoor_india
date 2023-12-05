c90c716d82ef1d88ad42ac2273d06067319c649c
Soumya <sambhuganguli1@gmail.com>
2023-12-05 17:01:16 +0530

    menu added

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const express = require("express");
 const app = express();
 const port = 5501;
 console.log(path.join(__dirname, '/public'));
+
 app.use(express.static(path.join(__dirname, '/public')))
+
+app.get("/menu", (req, res) =>{
+    res.sendFile("E:\\Programs\\Web Development\\tandoor_india\\public\\menu.html");
+})
 
 app.get("/", (req, res) => {
     res.send("hello express");
 })
 app.get("/booking", (req, res) => {
+    res.send("This will be booking page");
+})
+app.get("/menu", (req, res) => {
     res.send("This will be booking page");
 })
 
